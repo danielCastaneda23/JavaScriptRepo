8ff5c9d4177c7924e27179dee09d65ac2e20e741
danisper <u1802700@unimilitar.edu.co>
2021-01-07 22:09:58 -0500

    PrimerCommit

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Esto es una forma de programar*/
+//Otra forma 
+/*
+var ProductoUno ='Hola';
+var ProductoDos='ComoEstas';
+
+var Resultado =(ProductoUno +" "+ ProductoDos);
+document.write(Resultado);
+
+var numero=10;
+var animal =prompt("Digite Numero");
+document.write(Number(animal)+numero)
+*/
+
+var Numero=500;
+
+if(Numero<500){
+    alert("EsMenorA500")
+}else{
+    alert("NoesMenor")
+}
