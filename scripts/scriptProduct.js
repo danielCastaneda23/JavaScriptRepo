4ba2a3bdf628e0cccdb0e1576b9e0e858ff6cd32
danisper <u1802700@unimilitar.edu.co>
2021-02-26 16:03:33 -0500

    Ajax

--- scripts/scriptProduct.js
+++ scripts/scriptProduct.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
     acumulador = JSON.parse(localStorage.getItem('Carrito'));
     posicion=JSON.parse(localStorage.getItem('Position'));
     document.getElementById("ShowProduct").innerHTML = `
-        <div class="col-8 px-0">
-            <div class="row mx-0">
-                <img src="${acumulador[posicion]}" alt=":)" width="100%" height="100%">
-            </div>
-        </div>`;
+                <img src="${acumulador[posicion]}" alt=":)" width="600px" height="500px">
+            `;
 })
